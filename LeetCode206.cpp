15229fb64e8724e4e4635fc11faed6deaf485291
panyulin99 <413465199@qq.com>
2020-02-28 17:27:15 +0800

    反转链表

---
+++ LeetCode206.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode ret, *p = head, *q;
+    ret.next = NULL;
+    while (p) {
+        q = p->next;
+	p->next = ret.next;
+	ret.next = p;
+	p = q;
+    }
+    return ret.next;
+}
