2eab80adc4df1def8403e09270f8e4dedb7348ba
panyulin99 <413465199@qq.com>
2020-02-28 16:55:44 +0800

    delete

---
+++ LeetCode1.cpp
@@ -0,0 +1,19 @@
+/*
+* Note: The returned array must be malloced, assume caller calls free().
+*/
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *a = (int *)malloc(sizeof(int) * 2);
+    int i, j;
+    for (i = 0; i < numsSize - 1; i++) {
+	for (j = i + 1; j < numsSize; j++) {
+	    if (nums[i] + nums[j] == target) {
+	        a[0] = i;
+		a[1] = j;
+		*returnSize = 2;
+		return a;									              }
+         }
+     }               
+    *returnSize = 0; 
+    return a;
+}
---
+++ LeetCode160.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#define swap(a, b) {\
+    __typeof(a) __temp = a;\
+    a = b; b = __temp;\
+}
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    int cntA = 0, cntB = 0;
+    struct ListNode *p = headA, *q = headB;
+    while (p) cntA++, p = p->next;
+    while (q) cntB++, q = q->next;
+    int m = cntA - cntB;
+    p = headA, q = headB;
+    if (m < 0) {
+        swap(p, q);
+        m = -m;
+    }  
+    while (m--) p = p->next;
+    while (p != q) {
+        p = p->next;
+        q = q->next;
+    }
+    return p;
+}
+
+//下面的才是主要解法！！
+
+/*struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+     struct ListNode *p = headA, *q = headB;
+     while (p != q) {
+     p = p ? p->next : headB;
+     q = q ? q->next : headA;
+     }
+     return p;
+ }*/
